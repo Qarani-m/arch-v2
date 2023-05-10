380a084c44a30613523b23a60ef3e7e8d17b2403
Karani Martin <71381947+Qarani-m@users.noreply.github.com>
2023-05-10 16:23:11 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -2,16 +2,36 @@ import socket
 import threading
 import time
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = socket.gethostbyname(socket.gethostname())
-port = 9090
-header_len = 1024            
- 
-client.connect((ip, port))
 
+class Client:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.port = 9090
+        self.header_len = 1024
+        self.connect = self.client.connect((ip, port))
+    def recvMsg(self):
+        while True:
+            data=self.client.recv(header_len).decode()
+            if data.startswith("len"):
+                header_len=int(data[3:])
+                self.client.send("send".encode())
+    def sendData(self):
+        self.client.bind((self.ip,self.port))
+        self.client.listen(10)
+        print("send connection is open")
+        while True:
+            try:
+                conn, addr= self.server.accept()
+                print("got a connection from ", addr)
+                conn.send(("len"+str(self.getData()[0])).encode())
+                conn.close()
+            except Exception as e:
+                print(e)
 
-data =client.recv(header_len).decode()
+client =  Client()
+t1 = threading.Thread(target=client.sendData)
+t2 = threading.Thread(target=client.recvMsg)
 
-
-print(data)
-
+t1.start()
+t2.start()
--- server.py
+++ server.py
@@ -2,13 +2,14 @@ import socket
 import pyautogui as pag
 import time
 import threading
-port = 9090
-ip = socket.gethostbyname(socket.gethostname())
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 class Server:
     def __init__(self):
         self.byteSize = 64
+        self.port = 9090
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     def getData(self):
         time.sleep(10)
         pag.screenshot("file.png")
@@ -16,17 +17,29 @@ class Server:
             data = file.read()
         pic_length =len(data)
         return [pic_length]
+    def recvMsg(self):
+        self.server.connect((self.ip,self.port))
+        header_len= 2048
+        data = self.server.recv(header_len).decode
+        if data:
+            self.sendData
     def sendData(self):
-        server.bind((ip,port))
-        server.listen(10)
+        self.server.bind((self.ip,self.port))
+        self.server.listen(10)
         print("seeking connection")
         while True:
             try:
-                conn, addr= server.accept()
+                conn, addr= self.server.accept()
                 print("got a connection from ", addr)
-                conn.send(str(self.getData()[0]).encode())
+                conn.send(("len"+str(self.getData()[0])).encode())
                 conn.close()
             except Exception as e:
                 print(e)
 sender = Server()
-sender.sendData()
+t1 = threading.Thread(target=sender.sendData)
+
+t2 = threading.Thread(target=sender.recvMsg)
+
+t1.start()
+time.sleep(10)
+t2.start()
