020c2d0114f57fcfb5f33f4a3954b14762fe413f
Karani Martin <71381947+Qarani-m@users.noreply.github.com>
2023-05-25 12:01:42 +0300

    Update ph.py

--- ph.py
+++ ph.py
@@ -3,7 +3,7 @@ openai.api_key = "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"
 response = openai.Completion.create(
     engine='text-davinci-003',  # Specify the engine to use (e.g., text-davinci-003)
     prompt='Hello, how can I help you?',  # Provide a conversation prompt or message
-    max_tokens=50  # Set the maximum number of tokens in the response
+    max_tokens=50  # Set the maximum number of token in the response
 )
 
 reply = response.choices[0].text.strip()  # Extract the generated reply from the response
